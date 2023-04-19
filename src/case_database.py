d2ba13931210c239bbaa1b3203209b1e7d2ee23a
StarAria <stararia2021@126.com>
2023-04-19 23:43:40 +0800

    Class CaseData done

--- src/case_database.py
+++ src/case_database.py
@@ -12,7 +12,13 @@
 
 import numpy as np
 from scipy.sparse import *
+from scipy import constants
+from scipy.linalg import solve
 from scipy.sparse.linalg import splu
+
+# constant
+ERRORMAX = 1e10
+ERRORSAMPLECOUNT = 1000
 
 class CaseData(object):
     # Data for single case
@@ -72,7 +78,7 @@ class CaseData(object):
     def arnoldi(self, k, s):
         '''
         Arnoldi method to get the orthonormal matrix V
-        return -1 if the matrix d = g + s * c is singular
+        Return -1 if the matrix d = g + s * c is singular
         k: reduction order
         s: expansion point frequency
         '''
@@ -100,6 +106,96 @@ class CaseData(object):
 
         return v
     
+    def featureExtract(self):
+        '''
+        Extract the case features.
+        cMax, cMin, gMax, gMin: the max and min element in C and G matrix.
+        cMean, gMean: the mean value of C and G matrix.
+        cColVar, gColVar: the varience of column mean value of C and G matrix.
+        cNnz, gNnz: the none zero element count in C and G matrix.
+        cgElemProduct: the mean value of the product of each corresponding element in C and G matrix.
+        dim: the dimension of the C and G matrix.
+        '''
+        cMax = self.c.max()
+        gMax = self.g.max()
+        cMin = self.c.min()
+        gMin = self.g.min()
+        cMean = self.c.mean()
+        gMean = self.g.mean()
+        cColVar = np.var(self.c.mean(axis = 0))
+        gColVar = np.var(self.g.mean(axis = 0))
+        cNnz = self.c.nnz
+        gNnz = self.g.nnz
+        cgElemProduct = self.c.multiply(self.g).mean()
+        dim = self.order
+        return [cMax, gMax, cMin, gMin, cMean, gMean, cColVar, gColVar,\
+                cNnz, gNnz, cgElemProduct, dim]
+    
+    def errorEval(self, v, fLim, sampleCnt):
+        '''
+        Evaluate order reduction error with specific tranformation matrix V.
+        v: tranformation matrix given by Arnoldi method.
+        fLim: measurement frenquency upper limit.
+        sampleCnt: total measurement sample point count.
+        '''
+        rb = v.T * self.b
+        rc = v.T * self.c.todense() * v
+        rg = v.T * self.g.todense() * v
+
+        x = np.matrix(np.zeros((self.order, sampleCnt + 1)), dtype=complex)
+        rx = np.matrix(np.zeros((self.order, sampleCnt + 1)), dtype=complex)
+        step = fLim / sampleCnt
+
+        for i in range(0, sampleCnt + 1):
+            f = i * step
+            s = 2j * constants.pi * f
+            originSolver = splu(csc_matrix(self.g + s * self.c))
+            x[:, i] = originSolver.solve(self.b.todense())
+            t = solve((rg + s * rc), rb)
+            rx[:, i] = v * t
+
+        meanError = abs(x - rx).mean()
+        return meanError
+            
+    def measureMinError(self, totalOrder, maxFreq, step):
+        '''
+        Find the (frequence, order) of the 2nd expasion point
+        to minimum the total error of the 1st and 2nd expansion point.
+        The 1st expansion point is always at 0Hz.
+        totalOrder: the sum of the order after reducion on point 1 and 2.
+        maxFreq: maximum frequency of the 2nd expansion point.
+        step: sample step of the 2nd expansion point order.
+        '''
+        feature = self.featureExtract()
+        error = ERRORMAX
+
+        for i in range(1, totalOrder):
+            k1 = totalOrder - i
+            k2 = i
+            j = step
+            while j <= maxFreq:
+                s1 = 0
+                s2 = j
+                v1 = self.arnoldi(k1, s1) 
+                v2 = self.arnoldi(k2, s2)
+                if(type(v1) == int):
+                    error1 = ERRORMAX
+                else:
+                    error1 = self.errorEval(v1, maxFreq, ERRORSAMPLECOUNT)
+                if(type(v2) == int):
+                    error2 = ERRORMAX
+                else:
+                    error2 = self.errorEval(v2, maxFreq, ERRORSAMPLECOUNT)
+                if error1 + error2 <= error:
+                    error = error1 + error2
+                    result = feature + [i, j, error]
+                j = j + step
+
+        return result
+
+
+
+
 
 # ---------------------------------------------------------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------------------------------------------------------
@@ -126,9 +222,11 @@ if __name__ == "__main__":
     t.setC(c)
     t.setG(g)
 
-    v = t.arnoldi(4, 10e9)
+    v = t.arnoldi(5, 10e9)
 
-    print(v)
+    print(t.errorEval(v, 2e10, 1000))
+
+    print(t.measureMinError(15, 2e10, 1e9))
 
 
 
