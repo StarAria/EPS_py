117be75eae264f847a28b7731515189dc34866b7
StarAria <stararia2021@126.com>
2023-04-29 00:36:03 +0800

    Add tuning method and tune the model

--- src/predictor.py
+++ src/predictor.py
@@ -10,18 +10,32 @@
 # ---------------------------------------------------------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------------------------------------------------------
 
+import multiprocessing
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 
 # constant
 
 RESULT_FILE_NAME = "./result.txt"
 
+FREQ_CRITERION = 'gini'
+ORDER_CRITERION = 'gini'
+FREQ_ESTIMATORS = 300
+ORDER_ESTIMATORS = 300
+FERQ_MAX_DEPTH = 25  
+ORDER_MAX_DEPTH = 7
+FREQ_MIN_SAMPLES_SPLIT = 2
+ORDER_MIN_SAMPLES_SPLIT = 2
+FREQ_MIN_SAMPLES_LEAF = 1
+OERDER_MIN_SAMPLES_LEAF = 10
+
+
 class Predictor(object):
     '''
     Expansion point predictor using RandomForest algorythm.
     Need data from CaseDatabase.
-    Attributes: freqModel, orderModel, freqPredictingResult, orderPredictingResult
+    Attributes: freqModel, orderModel, freqPredictingResult, orderPredictingResult, tuning
     Function: buildModel, predict, showOobError, showPredictingResult, savePredictingResult
     '''
 
@@ -205,6 +219,190 @@ class Predictor(object):
 
         return
 
+    def tuning(self, trainingDataTable, item, paraName, paraLst = None):
+        '''
+        Tune the model.
+        '''
+        if(item not in ["frequency", "order"]):
+            raise ValueError("Unknown parameter \"item=" + item + "\"")
+        
+        if(type(trainingDataTable) != np.matrix):
+            trainingDataTable = np.matrix(trainingDataTable)
+        trainingLabel = trainingDataTable[:, 1:-3]
+
+        if(item == "frequency"):
+            trainingTarget = trainingDataTable[:, -2]
+            myCriterion = FREQ_CRITERION
+            myEstimators = FREQ_ESTIMATORS
+            myMaxDepth = FERQ_MAX_DEPTH
+            myMinSamplesSplit = FREQ_MIN_SAMPLES_SPLIT
+            myMinSamplesLeaf = FREQ_MIN_SAMPLES_LEAF
+        else:
+            trainingTarget = trainingDataTable[:, -3]
+            myCriterion = ORDER_CRITERION
+            myEstimators = ORDER_ESTIMATORS
+            myMaxDepth = ORDER_MAX_DEPTH
+            myMinSamplesSplit = ORDER_MIN_SAMPLES_SPLIT
+            myMinSamplesLeaf = OERDER_MIN_SAMPLES_LEAF
+
+        if(paraName == 'criterion'):
+            print("********Tune the criterion********\n")
+            giniScore = []
+            entropyScore = []
+            for i in range(100):
+                mdl = RandomForestClassifier(oob_score = True, criterion='gini')
+                mdl.fit(trainingLabel, np.ravel(trainingTarget))
+                giniScore.append(mdl.oob_score_)
+                mdl = RandomForestClassifier(oob_score = True, criterion='entropy')
+                mdl.fit(trainingLabel, np.ravel(trainingTarget))
+                entropyScore.append(mdl.oob_score_)
+            #print(giniScore)
+            #print(entropyScore)
+            print("gini score: " + str(np.mean(giniScore)))
+            print("entropy score: " + str(np.mean(entropyScore)))
+            print("...Done\n")
+
+        if(paraName == "n_estimators"):
+            print("********Tune the n_estimators********\n")
+            score = []
+            for i in paraLst:
+                buffer = []
+                for j in range(10):
+                    mdl = RandomForestClassifier(oob_score = True, criterion=myCriterion, n_estimators=i)
+                    mdl.fit(trainingLabel, np.ravel(trainingTarget))
+                    buffer.append(mdl.oob_score_)
+                score.append(np.mean(buffer))
+            plt.figure(paraName)
+            plt.title(item + "model")
+            plt.plot(paraLst, score, linewidth=1)
+            plt.xlabel(paraName)
+            plt.ylabel("score")
+            plt.savefig(paraName)
+            print("...Done\n")
+
+        if(paraName == "max_depth"):
+            print("********Tune the max_depth********\n")
+            score = []
+            for i in paraLst:
+                buffer = []
+                for j in range(10):
+                    mdl = RandomForestClassifier(oob_score = True, criterion=myCriterion, n_estimators=myEstimators,\
+                                                 max_depth=i)
+                    mdl.fit(trainingLabel, np.ravel(trainingTarget))
+                    buffer.append(mdl.oob_score_)
+                score.append(np.mean(buffer))
+            plt.figure(paraName)
+            plt.title(item + " model")
+            plt.plot(paraLst, score, linewidth=1)
+            plt.xlabel(paraName)
+            plt.ylabel("score")
+            plt.savefig(paraName)
+            print("...Done\n")
+
+        if(paraName == "min_samples_split"):
+            print("********Tune the min_samples_split********\n")
+            score = []
+            for i in paraLst:
+                buffer = []
+                for j in range(10):
+                    mdl = RandomForestClassifier(oob_score = True, criterion=myCriterion, n_estimators=myEstimators,\
+                                                 max_depth=myMaxDepth, min_samples_split=i)
+                    mdl.fit(trainingLabel, np.ravel(trainingTarget))
+                    buffer.append(mdl.oob_score_)
+                score.append(np.mean(buffer))
+            plt.figure(paraName)
+            plt.title(item + " model")
+            plt.plot(paraLst, score, linewidth=1)
+            plt.xlabel(paraName)
+            plt.ylabel("score")
+            plt.savefig(paraName)
+            print("...Done\n")
+
+        if(paraName == "min_samples_leaf"):
+            print("********Tune the min_samples_leaf********\n")
+            score = []
+            for i in paraLst:
+                buffer = []
+                for j in range(10):
+                    mdl = RandomForestClassifier(oob_score = True, criterion=myCriterion, n_estimators=myEstimators,\
+                                                 max_depth=myMaxDepth, min_samples_split=myMinSamplesSplit,\
+                                                 min_samples_leaf=i)
+                    mdl.fit(trainingLabel, np.ravel(trainingTarget))
+                    buffer.append(mdl.oob_score_)
+                score.append(np.mean(buffer))
+            plt.figure(paraName)
+            plt.title(item + " model")
+            plt.plot(paraLst, score, linewidth=1)
+            plt.xlabel(paraName)
+            plt.ylabel("score")
+            plt.savefig(paraName)
+            print("...Done\n")
+       
+        return
+
+    def _tryModel(self, para):
+        '''
+        Build model from paralst.
+        Used for parallel fine tune.
+        '''
+        [x, y, myCriterion, myEstimators, myMaxDepth, myMinSamplesSplit, myMinSamplesLeaf] = para
+        score = []
+        for i in range(100):
+            mdl = RandomForestClassifier(oob_score = True, criterion=myCriterion, n_estimators=myEstimators,\
+                                 max_depth=myMaxDepth, min_samples_split=myMinSamplesSplit,\
+                                 min_samples_leaf=myMinSamplesLeaf)
+            mdl.fit(x, np.ravel(y))
+            score.append(mdl.oob_score_)
+
+        return np.mean(score)
+    
+    def fineTune(self, item, trainingDataTable,\
+                 criterion_lst, n_estimators_lst, max_depth_lst, min_samples_split_lst, min_samples_leaf_lst):
+        '''
+        Fine tune the model.
+        Use grid search method.
+        '''
+
+        print("********Fine tune********\n")
+        if(item not in ["frequency", "order"]):
+            raise ValueError("Unknown parameter \"item=" + item + "\"")
+        
+        if(type(trainingDataTable) != np.matrix):
+            trainingDataTable = np.matrix(trainingDataTable)
+        x = trainingDataTable[:, 1:-3]
+
+        if(item == "frequency"):
+            y = trainingDataTable[:, -2]
+        else:
+            y = trainingDataTable[:, -3]
+
+        paras = []
+        for i in criterion_lst:
+            for j in n_estimators_lst:
+                for k in max_depth_lst:
+                    for m in min_samples_split_lst:
+                        for n in min_samples_leaf_lst:
+                            paras.append([x, y, i, j, k, m, n])
+        
+        # Create a pool of worker processes
+        with multiprocessing.Pool(processes=10) as pool:
+        # Apply the feature extract function to each training case in parallel
+            scores = pool.map(self._tryModel, paras)
+
+        print("mean score:" + str(np.mean(scores)) + "\n")
+
+        sorted_id = sorted(range(len(scores)), key = lambda k: scores[k], reverse = True)
+
+        print("Top 20 scores:")
+        print("\tscore\t\t\tcriterion\tn_estimators\tmax_depth\tmin_samples_split\tmin_samples_leaf")
+        for i in sorted_id[:20]:
+            print("\t" + str(scores[i]) + "\t" + str(paras[i][2]) + "\t\t" + str(paras[i][3]) +\
+                  "\t\t" + str(paras[i][4]) + "\t\t" + str(paras[i][5]) + "\t\t" + str(paras[i][6]))
+            
+        print("\n...Done\n")
+        return
+
+
 
 
 
@@ -222,7 +420,11 @@ if __name__ == "__main__":
     db = CaseDatabase(False)
     db.loadPredictingFeature()
     db.loadTrainingFeature()
-    predictor = Predictor(db)
+    predictor = Predictor()
+    # predictor.tuning(db.trainingFeatureTable(), "order", "min_samples_leaf", list(range(1, 20, 1)))
+    predictor.fineTune("frequency", db.trainingFeatureTable(),\
+                       ["gini"], [290, 295, 300, 305, 310], [21, 22, 23, 24, 25, 26, 27, 28, 29, 30],\
+                        [2, 3, 4], [1, 2, 3])
 
 
 
